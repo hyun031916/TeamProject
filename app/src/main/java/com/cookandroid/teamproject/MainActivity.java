4c92b22307b0de506d0b25dfdd7c4a03ba082377
ksh <hyun031916@gmail.com>
2020-09-11 03:36:21 +0900

    #2 로그인 화면 구성하기2

--- app/src/main/java/com/cookandroid/teamproject/MainActivity.java
+++ app/src/main/java/com/cookandroid/teamproject/MainActivity.java
@@ -3,7 +3,9 @@ package com.cookandroid.teamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         TextView idText = (TextView) findViewById(R.id.idText);
         TextView passwordText = (TextView) findViewById(R.id.passwordText);
         TextView welcome = (TextView) findViewById(R.id.WelcomeMessage);
+
+
 
 //        EditText edit1 = (EditText) findViewById(R.id.edit1);
 //        EditText edit2 = (EditText) findViewById(R.id.edit2);
